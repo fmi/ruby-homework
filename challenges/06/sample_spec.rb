2945203309d7585e4d7f4b7f31950faff60a2014
Alex Popov <alex.val.popov@gmail.com>
2013-11-05 12:11:05 +0200

    Add `to_s` methods
    When there are no to_s methods tests fail with:
    
    expected: "x^2 + 2"
                got: #<Polynomial:0x007fe365284f10 @coefficients="x^2 + 2">
    
    In the README.md there is also the to_s method:
    
        Polynomial.new([-3, -4, 1, 0, 6]).to_s #=> '- 3x^4 - 4x^3 + x^2 + 6'
        Polynomial.new([1, 0, 2]).to_s         #=> 'x^2 + 2'

--- challenges/06/sample_spec.rb
+++ challenges/06/sample_spec.rb
@@ -1,9 +1,9 @@
 describe Polynomial do
   it "formats a simple polynomial" do
-    expect(Polynomial.new([1, 0, 2])).to eq 'x^2 + 2'
+    expect(Polynomial.new([1, 0, 2]).to_s).to eq 'x^2 + 2'
   end
 
   it "formats a polynomial which contains negative and zero coefficients" do
-    expect(Polynomial.new([-3, -4, 1, 0, 6])).to eq '- 3x^4 - 4x^3 + x^2 + 6'
+    expect(Polynomial.new([-3, -4, 1, 0, 6]).to_s).to eq '- 3x^4 - 4x^3 + x^2 + 6'
   end
 end
