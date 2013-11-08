e25d76b78dc1146e77ac45bedfc2862aa36e4e00
Alex Popov <alex.val.popov@gmail.com>
2013-11-08 08:35:14 +0200

    Rewrote the specs for challenge 7 with `expect()`-syntax

--- challenges/07/sample_spec.rb
+++ challenges/07/sample_spec.rb
@@ -1,13 +1,13 @@
 describe "Bitmap" do
   it "renders bytes" do
-    Bitmap.new([9, 32], 1).render.should eq <<-ASCII.strip
+    expect(Bitmap.new([9, 32], 1).render).to eq <<-ASCII.strip
 ....#..#
 ..#.....
     ASCII
   end
 
   it "supports different palettes" do
-    Bitmap.new([13, 2, 5, 1], 2).render(%w[. * x #]).should eq <<-ASCII.strip
+    expect(Bitmap.new([13, 2, 5, 1], 2).render(%w[. * x #])).to eq <<-ASCII.strip
 ..#*...x
 ..**...*
     ASCII
