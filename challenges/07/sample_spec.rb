350cdc8d779220ebb586976e9b81125f3551dfb1
Stefan Kanev <stefan.kanev@gmail.com>
2013-11-09 23:50:32 +0200

    Revert "Rewrote the specs for challenge 7 with `expect()`-syntax"
    This reverts commit e25d76b78dc1146e77ac45bedfc2862aa36e4e00.
    
    Conflicts:
    	challenges/07/sample_spec.rb

--- challenges/07/sample_spec.rb
+++ challenges/07/sample_spec.rb
@@ -1,13 +1,13 @@
 describe "Bitmap" do
   it "renders bytes" do
-    expect(Bitmap.new([9, 32], 1).render).to eq <<-ASCII.strip
+    Bitmap.new([9, 32], 1).render.should eq <<-ASCII.strip
 ....#..#
 ..#.....
     ASCII
   end
 
   it "supports different palettes" do
-    expect(Bitmap.new([13, 2, 5, 1], 2).render(%w(. * x #))).to eq <<-ASCII.strip
+    Bitmap.new([13, 2, 5, 1], 2).render(%w[. * x #]).should eq <<-ASCII.strip
 ..#*...x
 ..**...*
     ASCII
