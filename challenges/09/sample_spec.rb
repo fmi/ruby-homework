22b86ea1eff0008dc4a121884b2367d8d2270fda
Plamen Stoev <psstoev@gmail.com>
2013-11-15 17:38:49 +0200

    Add the next challenge - Memoizer

---
+++ challenges/09/sample_spec.rb
@@ -0,0 +1,9 @@
+require './spec_helper.rb'
+
+describe "Memoizer" do
+  it "calls the methods of the target class" do
+    memoizer = Memoizer.new "Foo"
+
+    memoizer.*(3).should eq "FooFooFoo"
+  end
+end
