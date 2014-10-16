e41a961dab3e1fcf2efcbda213076fcffb379bdc
Dimitar Dimitrov <me@ddimitrov.name>
2014-10-17 00:48:48 +0300

    Add challenge 02

---
+++ challenges/02/sample_spec.rb
@@ -0,0 +1,13 @@
+describe 'tic_tac_toe_board_from' do
+  it 'renders boards with x and o markers' do
+    expect(render_tic_tac_toe_board_to_ascii([
+      :x,  nil, nil,
+      :o,  :x,  nil,
+      :x,  :o,  :o,
+    ])).to eq(<<BOARD.chomp)
+| x |   |   |
+| o | x |   |
+| x | o | o |
+BOARD
+  end
+end
