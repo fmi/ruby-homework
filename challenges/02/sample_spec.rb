0543d48bb7ab4038d11caeac7d831e72d8366016
Georgy Angelov <georgyangelov@gmail.com>
2016-09-24 21:31:01 +0300

    Prepare for 2016

--- challenges/02/sample_spec.rb
+++
@@ -1,6 +0,0 @@
-describe 'button_presses' do
-  it 'counts the number of pressed buttons' do
-    expect(button_presses('nvm')).to eq 6
-    expect(button_presses('WHERE DO U WANT 2 MEET L8R')).to eq 47
-  end
-end
